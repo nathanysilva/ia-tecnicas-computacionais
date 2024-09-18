e6fad86cc4d426bd2d40fef40f42870153a71e92
nathanysilva <110236003+nathanysilva@users.noreply.github.com>
2024-09-18 08:58:30 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,2 +1,9 @@
 const nomes = ( "Roger","Nathy", "Ronaldo", "Katiane", "Cesar","Debora", "Xibrow")
 
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
+
